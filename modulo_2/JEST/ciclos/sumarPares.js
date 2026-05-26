2eff9b6bc21f75e0b473d249577501ca42d525cf
hhhernan234 <hhhernan234>
2026-05-26 08:29:57 -0500

    Clase 26-05-26

---
+++ modulo_2/JEST/ciclos/sumarPares.js
@@ -0,0 +1,16 @@
+function sumarPares(numeros) {
+    if(!Array.isArray(numeros) || numeros.length === 0)
+        throw new TypeError('Arreglo no valido');
+    let contador = 0;
+    for(let i = 0; i < numeros.length-1; i++) {
+        if (numeros[i] % 2 === 0){
+        contador++;
+        }
+    }
+    return contador;
+}
+module.exports = { sumarPares };
+
+
+
+
